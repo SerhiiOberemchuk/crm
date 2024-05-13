71afd5ef1c8b9031e4dfe59e244f3db6585e385d
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-05-13 22:20:53 +0200

    add modall company button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-import StatusLabel, { Status } from '@/app/components/status-label';
-import { headers } from 'next/headers';
+import AddCompanyButton from './components/add-company-button';
 
 export default function Home() {
-  console.log(headers());
-
   return (
     <main>
-      <h1 className="text-xl">Home page{new Date().toString()}</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <h1 className="text-xl">Home page</h1>
+      <AddCompanyButton />
     </main>
   );
 }
